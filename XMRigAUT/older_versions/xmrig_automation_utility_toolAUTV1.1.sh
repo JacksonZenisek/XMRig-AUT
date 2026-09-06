ada928defa59fce45e1677eac5228aa4e110342b
Jackson Zenisek <115522316+JacksonZenisek@users.noreply.github.com>
2026-09-06 07:42:20 -0500

    Update xmrig_automation_utility_toolAUTV1.1.sh

--- XMRigAUT/older_versions/xmrig_automation_utility_toolAUTV1.1.sh
+++ XMRigAUT/older_versions/xmrig_automation_utility_toolAUTV1.1.sh
@@ -2,7 +2,7 @@
 # A hopefully helpful utility tool for xmrig miners.
 # Developed by Jackson Zenisek
 # V1.1
-# Last updated: 8/27/2026
+# Release date: 8/27/2026
 
 mainmenu() {
 echo "----------------------------"
